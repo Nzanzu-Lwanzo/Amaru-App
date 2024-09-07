6ce1ca60bfe09bad57ecfebe3a0dbb4b423bf5da
Nzanzu Lwanzo <nzanzu.lwanzo.work@gmail.com>
2024-09-07 14:46:23 +0200

    Image routing added

--- blog/staticfiles/post/js/read.js
+++ blog/staticfiles/post/js/read.js
@@ -2,5 +2,6 @@ const matches = matchMedia("((max-width: 490px))").matches;
 const article = document.querySelector("article");
 
 (function () {
-  article.classList.add("article-read")
+  article.classList.add("article-read");
+  document.querySelector("p[data-f-id='pbf']")?.remove()
 })();
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +11,10 @@ urlpatterns = [
     path('',include("standalone.urls")),
     path('manage/',include("manager.urls")),
     path('froala_editor/', include('froala_editor.urls')),
-]
+
+    # ADDED FOLLOWING THE INSTRUCTIONS OF
+    # https://dev.to/jmegnidro/deploying-a-django-application-on-cpanel-beginners-guide-54km
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root' : settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$',serve,{'document_root' : settings.STATIC_ROOT})
+    
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
